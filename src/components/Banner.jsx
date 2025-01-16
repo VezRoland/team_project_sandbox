5c5b07fdbd235080b7334e6b4f3e4d6234127f17
MountainSpiritq <greenbull122@gmail.com>
2025-01-16 10:42:23 +0100

    bannerstyling

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const Banner = () => {
   return (
-    <div className='banner'>
-      <h1 style={{textAlign:"center"}}>Pizza rendelés gyorsan, kénylemesen</h1>
+    <div className='banner flex justify-center items-center'>
+      <div className="box h-fit w-screen p-5 bg-slate-900/50">
+      <h1 className='text-3xl animate-pulse' style={{textAlign:"center"}}>Pizza rendelés gyorsan, kénylemesen</h1>
+
+      </div>
   </div>
   )
 }
